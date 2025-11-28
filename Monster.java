a20a774e923cd90745261ee1e92eb70bcf4af801
Gianne-Hatdog <heikinnshi@gmail.com>
2025-11-28 21:42:32 +0800

    Update Monster.java

--- Monster.java
+++ Monster.java
@@ -1,18 +1,19 @@
 public class Monster extends Character {
+
     public Monster(String name, int health, int attackDamage, int defense) {
-        super(name, health, attackDamage, defense);
+        super(name, health, attackDamage, defense);  // Basic monster setup
     }
 
     @Override
     public void attack(Character target) {
         System.out.println(this.getName() + " attacks " + target.getName());
-        target.takeDamage(this.getAttackDamage());
+        target.takeDamage(this.getAttackDamage());   // Normal attack
     }
 
     @Override
     public boolean specialAttack(Character target) {
         System.out.println(this.getName() + " uses Ferocious Bite!");
-        target.takeDamage(this.getAttackDamage() + 10);
-        return true;
+        target.takeDamage(this.getAttackDamage() + 10);  // Stronger bite
+        return true; // Monster always succeeds
     }
 }
